edf4a4908f2b23925d3b5ef9435b3a863b62c5b2
Neetshin <n33t5hin@gmail.com>
2019-11-19 15:24:10 +0900

    FIx

--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,5 @@
 const greeting = require('./');
 
 test('salutates properly', () => {
-  expect(greeting('Ryo')).toBe('This won\'t work properly');
+  expect(greeting('Ryo')).toBe('Hello Ryo!');
 });
